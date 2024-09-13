4d72a496c8ba7fa94af1d392d85c66377e53d3fa
RameesMhd <muhammed.ramees@neopraxis.in>
2024-09-13 11:12:45 +0530

    working frontend

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,54 +13,40 @@ export default function Todo() {
 
     // Fetch todos from the backend
     useEffect(() => {
-        const fetchTodos = async () => {
-            try {
-                const response = await axios.get(`${apiUrl}/todos/${currentUser.uid}`);
-                setTodos(response.data);
-            } catch (error) {
-                console.error('Error fetching todos:', error);
-            }
-        };
+        async function fetchTodos() {
+            const response = await axios.get(`${apiUrl}/todos/${currentUser.uid}`);
+            setTodos(response.data);
+        }
         fetchTodos();
-    }, [currentUser, apiUrl]);
+    }, [currentUser]);
 
     // Add a new todo
     const handleAddTodo = async () => {
-        try {
-            const response = await axios.post(`${apiUrl}/todos`, {
-                userId: currentUser.uid,
-                title: newTodo,
-            });
-            setTodos([...todos, response.data]);
-            setNewTodo('');
-        } catch (error) {
-            console.error('Error adding todo:', error);
-        }
+        const response = await axios.post(apiUrl, {
+            userId: currentUser.uid,
+            title: newTodo,
+        });
+        setTodos([...todos, response.data]);
+        setNewTodo('');
     };
 
-    // Toggle complete status of a todo
-    const handleToggleComplete = async (id) => {
-        try {
-            const response = await axios.patch(`${apiUrl}/todos/${id}`);
-            setTodos(todos.map((todo) => (todo._id === id ? response.data : todo)));
-        } catch (error) {
-            console.error('Error toggling complete status:', error);
-        }
+    // Toggle complete status of a todo (mark as active or completed)
+    const handleToggleComplete = async (id, completed) => {
+        const response = await axios.patch(`${apiUrl}/todos/${id}`, {
+            completed: !completed,
+        });
+        setTodos(todos.map(todo => (todo._id === id ? response.data : todo)));
     };
 
     // Delete a todo
-    const handleDeleteTodo = async (id) => {
-        try {
-            await axios.delete(`${apiUrl}/todos/${id}`);
-            setTodos(todos.filter((todo) => todo._id !== id));
-        } catch (error) {
-            console.error('Error deleting todo:', error);
-        }
+    const handleDeleteTodo = async id => {
+        await axios.delete(`${apiUrl}/todos/${id}`);
+        setTodos(todos.filter(todo => todo._id !== id));
     };
 
     // Filter the todos into active and completed sections
-    const activeTodos = todos.filter((todo) => !todo.completed);
-    const completedTodos = todos.filter((todo) => todo.completed);
+    const activeTodos = todos.filter(todo => !todo.completed);
+    const completedTodos = todos.filter(todo => todo.completed);
 
     return (
         <div className="todo-container container">
@@ -78,7 +64,7 @@ export default function Todo() {
                     className="todo-input"
                     type="text"
                     value={newTodo}
-                    onChange={(e) => setNewTodo(e.target.value)}
+                    onChange={e => setNewTodo(e.target.value)}
                     placeholder="New Todo"
                 />
                 <button
@@ -91,44 +77,40 @@ export default function Todo() {
             </div>
 
             {/* Active Todos List */}
-            {todos.length > 0 && (
-                <>
-                    <h2 className='header'>Active Todos</h2>
-                    <ul className="todo-list">
-                        {activeTodos.map((todo) => (
-                            <li key={todo._id} className="todo-item">
-                                <span
-                                    onClick={() => handleToggleComplete(todo._id)}
-                                    className="todo-title"
-                                >
-                                    {todo.title}
-                                </span>
-                                <div className='btn-wrapper'>
-                                    <button
-                                        className="todo-complete-button btn btn-success"
-                                        onClick={() => handleToggleComplete(todo._id)}
-                                    >
-                                        Done
-                                    </button>
-                                    <button
-                                        className="todo-delete-button btn btn-warning"
-                                        onClick={() => handleDeleteTodo(todo._id)}
-                                    >
-                                        Delete
-                                    </button>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            )}
+            <h2 className='header'>Active Todos</h2>
+            <ul className="todo-list">
+                {activeTodos.map(todo => (
+                    <li key={todo._id} className="todo-item">
+                        <span
+                            onClick={() => handleToggleComplete(todo._id, todo.completed)}
+                            className="todo-title"
+                        >
+                            {todo.title}
+                        </span>
+                        <div className='btn-wrapper'>
+                            <button
+                                className="todo-complete-button btn btn-success"
+                                onClick={() => handleToggleComplete(todo._id, todo.completed)}
+                            >
+                                Done
+                            </button>
+                            <button
+                                className="todo-delete-button btn btn-warning"
+                                onClick={() => handleDeleteTodo(todo._id)}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
 
             {/* Completed Todos Section */}
             {completedTodos.length > 0 && (
                 <>
                     <h2 className='header'>Completed Todos</h2>
                     <ul className="todo-list">
-                        {completedTodos.map((todo) => (
+                        {completedTodos.map(todo => (
                             <li key={todo._id} className="todo-item">
                                 <span className="todo-title completed">
                                     {todo.title}
@@ -136,7 +118,7 @@ export default function Todo() {
                                 <div className='btn-wrapper'>
                                     <button
                                         className="todo-active-button btn btn-primary"
-                                        onClick={() => handleToggleComplete(todo._id)}
+                                        onClick={() => handleToggleComplete(todo._id, todo.completed)}
                                     >
                                         Not Done
                                     </button>
