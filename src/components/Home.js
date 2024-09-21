db0aa6bd9f53aff172dd98fb990e4c0b1b0ece08
RameesMhd <muhammed.ramees@neopraxis.in>
2024-09-21 16:56:53 +0530

    homepage styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,22 @@
 // src/components/Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './CommonStyle.scss';
 
 function Home() {
   return (
-    <div className='wrapper container mt-5'>
-      <h1 className='text-center'>Welcome to the Todo App!</h1>
-      <p className='text-center'>Please log in or register to continue.</p>
-      <div className='btn-wrapper'>
-        <Link to="/login">
-          <button className='btn btn-primary'>Login</button>
-        </Link>
-        <Link to="/register">
-          <button className='btn btn-primary'>Register</button>
-        </Link>
+    <div className='home-page'>
+      <div className='home-page-wrapper'>
+        <h1 className='text-center'>Welcome to the Todo App!</h1>
+        <p className='text-center'>Please log in or register to continue.</p>
+        <div className='btn-wrapper'>
+          <Link className='btn btn-primary' to="/login">
+            Login
+          </Link>
+          <Link className='btn btn-primary' to="/register">
+            Register
+          </Link>
+        </div>
       </div>
     </div>
   );
